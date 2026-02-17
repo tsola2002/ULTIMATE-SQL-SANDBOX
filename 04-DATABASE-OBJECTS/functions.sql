5edbad98c43fcab670b93106bc6e8fae273d5694
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-02-17 12:41:25 +0100

    database objects

--- 04-DATABASE-OBJECTS/functions.sql
+++ 04-DATABASE-OBJECTS/functions.sql
@@ -1,3 +1,18 @@
+-- creating a function to add two numbers together
+CREATE FUNCTION add_numbers(num1 INT, num2 INT)
+RETURNS INT
+RETURN num1 + num2;
+
+DELIMITER //
+CREATE FUNCTION subtract_numbers(num1 INT, num2 INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+RETURN num1 - num2;
+END //
+DELIMITER;
+
+
 -- this will create a function to round a number
 DROP FUNCTION IF EXISTS rndb;
 CREATE FUNCTION rndb (original FLOAT, basis FLOAT)
@@ -5,7 +20,71 @@ RETURNS FLOAT
 RETURN TRUNCATE(original / basis + 0.5, 0) * basis;
 
 -- this will display all functions in a specified database
-SHOW FUNCTION STATUS WHERE Db = 'sakila';
+SHOW FUNCTION STATUS WHERE Db = 'world';
+
+-- this will call a user defined function
+SELECT add_numbers(10, 5);
+SELECT subtract_numbers(10, 5);
+
+-- this will delete a function
+DROP FUNCTION subtract_numbers;
+
+-- function to get number of cities in a country
+DELIMITER //
+CREATE FUNCTION get_city_count(country_code CHAR(3))
+RETURNS INT
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+    DECLARE city_total INT;
+
+    SELECT COUNT(*)
+    INTO city_total
+    FROM city
+    WHERE CountryCode = country_code;
+
+    RETURN city_total;
+END //
+
+DELIMITER;
+
+
+SELECT get_city_count('USA');
+
+DELIMITER //
+
+CREATE FUNCTION get_grades(score INT)
+RETURNS VARCHAR(20)
+DETERMINISTIC
+BEGIN
+    DECLARE grade VARCHAR(20);
+
+    IF score >= 70 THEN
+        SET grade = 'A';
+    ELSEIF score >= 60 THEN
+        SET grade = 'B';
+    ELSE
+        SET grade = 'C';
+    END IF;
+
+    RETURN grade;
+END //
+
+DELIMITER;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 SELECT TRUNCATE(27.88/0.25+ 0.5, 0) * 0.25;
 
--- 04-DATABASE-OBJECTS/views.sql
+++ 04-DATABASE-OBJECTS/views.sql
@@ -1,3 +1,30 @@
+CREATE VIEW `african_countries` AS
+SELECT Code, Name, Population
+FROM country
+WHERE Continent = 'Africa';
+
+-- this code will list all the view in your database
+SHOW FULL TABLES IN world WHERE Table_type LIKE 'VIEW';
+
+-- this will run and excute your views
+SELECT * FROM african_countries;
+
+-- this will delete a specified view
+DROP VIEW african_countries;
+
+-- this will check if a view is updatable
+SELECT table_name, is_updatable 
+FROM information_schema.views
+WHERE table_schema = 'world'
+AND table_name = 'african_countries';
+
+-- this will allow you to update a view
+UPDATE african_countries
+SET Population = 5000000;
+WHERE Code = 'NGA';
+
+
+
 -- this code will create a view
 CREATE VIEW `customerLogDates` AS
 SELECT first_name, last_name, email, create_date FROM customer;
@@ -5,22 +32,18 @@ SELECT first_name, last_name, email, create_date FROM customer;
 CREATE VIEW `customershortlog` AS
 SELECT first_name, last_name, email, create_date FROM customer LIMIT 10;
 
--- this code will list all the view in your database
-SHOW FULL TABLES IN sakila WHERE Table_type LIKE 'VIEW';
+
 
 -- this will select all information from customerlogdates view
 SELECT * FROM customerlogdates;
 
--- this will check if a view is updatable
-SELECT table_name, is_updatable FROM views
-ORDER BY table_name;
+
 
 -- this will update an existing view
 CREATE OR REPLACE VIEW customerlogdates AS
 SELECT first_name, last_name FROM customer;
 
--- this will delete a specified view
-DROP VIEW customerlogdates;
+
 
 -- this will delete more than one view
 DROP VIEW customerdata, actor_info;
